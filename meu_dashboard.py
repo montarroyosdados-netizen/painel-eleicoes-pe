5b6bd06958098519dc00f5ad21558ac4ec3d425a
Guilherme Montarroyos <montarroyos.dados@gmail.com>
2026-07-22 15:03:17 -0300

    Restaurando codigo original sem enfeites

--- meu_dashboard.py
+++ meu_dashboard.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Painel Eleitoral de Pernambuco", layout="wide")
 
-st.title("🦁 Painel Eleitoral de Pernambuco")
+st.title("Painel Eleitoral de Pernambuco")
 
 
 @st.cache_data
